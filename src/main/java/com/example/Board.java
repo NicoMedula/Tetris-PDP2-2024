bbb26bb1a8834d112c862e9aaea881d240b2540b
NicoMedula <nicolasmedula@hotmail.com>
2024-08-30 08:27:38 -0300

    Posibles manera de realizar el tablero

--- src/main/java/com/example/Board.java
+++ src/main/java/com/example/Board.java
@@ -1,12 +1,47 @@
 package com.example;
 
+import java.util.Random;
+
+import com.example.Pieces.PieceDogLeft;
+import com.example.Pieces.PieceDogRight;
+import com.example.Pieces.PieceEleLeft;
+import com.example.Pieces.PieceEleRight;
+import com.example.Pieces.PieceSquare;
+import com.example.Pieces.PieceStick;
+import com.example.Pieces.PieceT;
+
 public class Board {
 
     private int board[][];
+    private Random random = new Random();
+    
 
     public void setBoard() {
         board = new int[10][20];
     }
 
+    public void agregarPiezaRandom(){
+        
+        int randomPieza = random.nextInt(7);
+
+        IPiece pieza = null;
+
+        switch(randomPieza){
+            case 0: pieza = new PieceSquare(); break;
+
+            case 1: pieza = new PieceT(); break;
+
+            case 2: pieza = new PieceStick(); break;
+
+            case 3: pieza = new PieceEleLeft(); break;
+
+            case 4: pieza = new PieceEleRight(); break;
+
+            case 5: pieza = new PieceDogLeft(); break;
+            
+            case 6: pieza = new PieceDogRight(); break;
+        }
+    }
+    
     
 }
